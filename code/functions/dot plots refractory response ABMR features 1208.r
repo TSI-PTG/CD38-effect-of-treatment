163b8a83089f3215a893a122c9f117a2f9e698b5
TSI-PTG <pgauthi1@ualberta.ca>
2024-05-24 08:11:28 -0600

    .

--- code/functions/dot plots refractory response ABMR features 1208.r
+++ code/functions/dot plots refractory response ABMR features 1208.r
@@ -14,14 +14,17 @@ load("Z:/MISC/Phil/AA All papers in progress/A GC papers/AP1.0A CD38 molecular e
 simplefile <- read_excel("Z:/MISC/Phil/AA All papers in progress/A GC papers/0000 simple XL files/Kidney 5086/MASTER COPY K5086 SimpleCorrAAInjRej 5AAInjNR 7AARej.xlsx")
 
 
+# CHECK THE CATEGORIES ####
+felzartamab_ARTanova$category
+
+
 # DEFINE THE ABMR SCORES ####
 scores_ABMR <- felzartamab_ARTanova %>%
-    dplyr::filter(category %in% c("cfDNA", "ABMR")) %>%
+    dplyr::filter(category %in% c("cfDNA", "ABMR", "injury", "archetypes", "rejectionPC", "injuryPC")) %>%
     dplyr::select(score, variable, medians_delta) %>%
     unnest(everything()) %>%
     dplyr::filter(Followup_pairwise == "Baseline - Week52") %>%
     distinct(variable, .keep_all = TRUE) %>%
     dplyr::select(score, variable, Followup_pairwise, median_delta_delta)
 
-
-scores_ABMR$medians_delta[[1]]
+scores_ABMR$median_delta_delta[[1]]
