ea3a0f83278156c3c959522c35f4d9420f8b74af
TSI-PTG <pgauthi1@ualberta.ca>
2024-06-12 10:46:10 -0600

    .

--- code/tables/NK and endothelial selective genes 1208.r
+++ code/tables/NK and endothelial selective genes 1208.r
@@ -18,35 +18,34 @@ load("Z:/MISC/Phil/AA All papers in progress/A GC papers/AP1.0A CD38 molecular e
 # load("Z:/MISC/Phil/AA All papers in progress/A GC papers/AP1.0A CD38 molecular effects Matthias PFH/data/NK cell selective genes.RData")
 load("Z:/MISC/Phil/AA All papers in progress/A GC papers/AP1.0A CD38 molecular effects Matthias PFH/data/NK_genes_L765.RData")
 load("Z:/MISC/Phil/AA All papers in progress/A GC papers/AP1.0A CD38 molecular effects Matthias PFH/data/ABMR_endothelial_genes.RData")
+load("Z:/MISC/Phil/AA All papers in progress/A GC papers/AP1.0A CD38 molecular effects Matthias PFH/data/ABMR_activity_genes.RData")
 
 
 # FILTER THE GENE TABLES ####
 gene_tables <- limma_tables %>%
     dplyr::select(design, table) %>%
+    expand_grid(geneset = c("ABMR_activity", "NK", "Endothelial")) %>%
     mutate(
-        tables_nk = map(
-            table,
-            function(table) {
+        genes = case_when(
+            geneset == "ABMR_activity" ~ genes_ABMR_activity$AffyID %>% list(),
+            geneset == "NK" ~ genes_NK$AffyID %>% list(),
+            geneset == "Endothelial" ~ genes_ABMR_endothelial$AffyID %>% list()
+        )
+    ) %>%
+    relocate(geneset, genes, .after = design) %>%
+    mutate(
+        gene_tables = pmap(
+            list(table, genes),
+            function(table, genes) {
                 table %>%
-                    dplyr::filter(AffyID %in% genes_NK$AffyID) %>%
-                    dplyr::slice_min(`<U+0394><U+0394> p`, by = "Symb")
-            }
-        ),
-        tables_endothelial = map(
-            table,
-            function(table) {
-                table %>%
-                    dplyr::filter(AffyID %in% genes_ABMR_endothelial$AffyID) %>%
+                    dplyr::filter(AffyID %in% genes) %>%
                     dplyr::slice_min(`<U+0394><U+0394> p`, by = "Symb")
             }
         )
     )
-names(gene_tables$tables_nk) <- gene_tables$design
-names(gene_tables$tables_endothelial) <- gene_tables$design
 
-
-gene_tables$tables_nk[[1]]
-gene_tables$tables_endothelial[[1]]
+names(gene_tables$gene_tables) <-gene_tables$design
+gene_tables$gene_tables
 
 
 # EXPORT THE DATA AS .RData FILE ####
@@ -54,23 +53,43 @@ saveDir <- "Z:/MISC/Phil/AA All papers in progress/A GC papers/AP1.0A CD38 molec
 save(gene_tables, file = paste(saveDir, "gene_tables_limma_1208.RData", sep = ""))
 
 
+# SPLIT GENE TABLES BY GENESET FOR EXPORTING TABLES ####
+gene_tables_ABMR_activity <- gene_tables %>% dplyr::filter(geneset == "ABMR_activity")
+gene_tables_NK <- gene_tables %>% dplyr::filter(geneset == "NK")
+gene_tables_Endothelial <- gene_tables %>% dplyr::filter(geneset == "Endothelial")
+
+
+
 # EXPORT THE DATA AS AN EXCEL SHEET ####
 saveDir1 <- "Z:/MISC/Phil/AA All papers in progress/A GC papers/AP1.0A CD38 molecular effects Matthias PFH/output/"
-openxlsx::write.xlsx(gene_tables$tables_nk,
+openxlsx::write.xlsx(gene_tables_ABMR_activity$gene_tables,
     asTable = TRUE,
-    file = paste(saveDir1, "NK_genes_limma_1208_11Jun24",
+    file = paste(saveDir1, "ABMR_activity_genes_limma_1208_12Jun24",
         # Sys.Date(),
         # format(Sys.time(), "_%I%M%p"),
         ".xlsx",
         sep = ""
     )
 )
-openxlsx::write.xlsx(gene_tables$tables_endothelial,
+openxlsx::write.xlsx(gene_tables_NK$gene_tables,
     asTable = TRUE,
-    file = paste(saveDir1, "Endothelial_genes_limma_1208_11Jun24",
+    file = paste(saveDir1, "NK_genes_limma_1208_12Jun24",
         # Sys.Date(),
         # format(Sys.time(), "_%I%M%p"),
         ".xlsx",
         sep = ""
     )
 )
+openxlsx::write.xlsx(gene_tables_Endothelial$gene_tables,
+    asTable = TRUE,
+    file = paste(saveDir1, "Endothelial_genes_limma_1208_12Jun24",
+        # Sys.Date(),
+        # format(Sys.time(), "_%I%M%p"),
+        ".xlsx",
+        sep = ""
+    )
+)
+
+
+
+
