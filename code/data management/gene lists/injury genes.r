42c855506242a3b61036cbdce0b2431909327f8c
TSI-PTG <pgauthi1@ualberta.ca>
2024-07-04 17:17:37 -0600

    .

--- code/data management/gene lists/injury genes.r
+++ code/data management/gene lists/injury genes.r
@@ -16,45 +16,35 @@ simplefile_path <- "Z:/MISC/Phil/AA All papers in progress/A GC papers/0000 simp
 simplefile <- read_excel(path = simplefile_path, sheet = "simpleCorrAAInjRejInjset")
 
 
-
 genes_injury_markers %>%
-    mutate(data = map(data, filter, abs(log2FC) > 1)) %>%
+    # mutate(data = map(data, filter, abs(log2FC) > 1)) %>%
     unnest(data) %>%
     drop_na(AffyID) %>%
-    filter(Symb == "VIM") %>%
-    dplyr::select(AffyID, Symb, cluster)
-
-
+    filter(Symb == "SLFN5") %>%
+    dplyr::select(AffyID, Symb, cluster, log2FC)
 
 
 # WRANGLE THE INJURY MARKER DATA ####
 injury_markers <- genes_injury_markers %>%
-    mutate(data = map(data, filter, abs(log2FC) > 1)) %>%
+    # mutate(data = map(data, filter, abs(log2FC) > 1)) %>%
     unnest(data) %>%
     drop_na(AffyID) %>%
     dplyr::filter(cluster %>% str_detect("New")) %>%
     dplyr::select(cluster, AffyID, Symb, Gene, PBT) %>%
     nest(.by = AffyID) %>%
-    # mutate(
-    #     "cellular expression" = map_chr(
-    #         data,
-    #         function(data) {
-    #             data %>%
-    #                 pull(cluster) %>%
-    #                 paste(collapse = ",")
-    #         }
-    #     )
-    # ) %>%
+    mutate(
+        "cellular expression" = map_chr(
+            data,
+            function(data) {
+                data %>%
+                    pull(cluster) %>%
+                    paste(collapse = ",")
+            }
+        )
+    ) %>%
     unnest(data) %>%
-    distinct(Symb, .keep_all = TRUE) %>%
+    # distinct(Symb, .keep_all = TRUE) %>%
     dplyr::select(-cluster)
-
-
-injury_markers %>%
-    left_join(K4502, by = "AffyID") %>%
-    dplyr::filter(Symb == "VIM") %>%
-    dplyr::select(AffyID, Symb, cluster)
-
 
 
 # WRANGLE THE SIMPLE FILE DATA ####
@@ -68,7 +58,6 @@ K4502 <- simplefile %>%
         AffyID = Affy,
         Symb = SYMB
     ) %>%
-    # dplyr::slice_min(`pvalInjAA4-AKI2`, by = "SYMB") %>%
     dplyr::select(
         AffyID,
         `corrInjAA3-AKI1`, `pvalInjAA3-AKI1`, `corrInjAA4-AKI2`, `pvalInjAA4-AKI2`,
@@ -81,37 +70,50 @@ K4502 <- simplefile %>%
 
 
 # JOIN THE INJURY MARKER DATA WITH THE SIMPLEFILE DATA ####
-injury_markers %>%
-    left_join(K4502, by = "AffyID") %>%
-        dplyr::filter(Symb == "VIM") %>%
-        dplyr::select(AffyID, Symb)
-
-
-
-
 join <- injury_markers %>%
     left_join(K4502, by = "AffyID") %>%
-    distinct(Symb, .keep_all = TRUE) %>%
-    dplyr::select(-AffyID)
-
-
-injury_markers %>%
-    dplyr::filter(Symb == "VIM") %>%
-    dplyr::select(AffyID, Symb)
+    dplyr::select(-AffyID) %>%
+    dplyr::slice_max(`corrInjAA4-AKI2`, by = "Symb") %>%
+    distinct(Symb, .keep_all = TRUE)
 
 
 
-join %>%
-    dplyr::filter(Symb == "VIM") %>%
-    dplyr::select(AffyID, Symb)
+
 
 # DEFINE THE INJURY GENES BY AKI1 AND AKI2 ASSOCIATION ####
 genes_injury <- join %>%
-    dplyr::filter(`corrInjAA3-AKI1` < 0 & `corrInjAA4-AKI2` < 0)
+    dplyr::filter(
+        # `corrInjAA3-AKI1` > 0,
+        # `pvalInjAA3-AKI1` < 0.05,
+        # `corrInjAA4-AKI2` > 0,
+        # `pvalInjAA4-AKI2` < 0.05
+    ) %>%
+    mutate(
+        AKI1_rank = rank(`corrInjAA3-AKI1` %>% desc(), ties.method = "average"),
+        AKI2_rank = rank(`corrInjAA4-AKI2` %>% desc(), ties.method = "average"),
+        PC1_rank = rank(corrInjPCA1 %>% desc(), ties.method = "average"),
+        PC2_rank = rank(corrInjPCA2, ties.method = "average"),
+        rank = rank(AKI2_rank + PC2_rank, ties.method = "average"),
+        .before = 1
+    ) %>%
+    arrange(rank)
 
 
 
-genes_injury %>% print(n = "all")
+genes_injury %>%
+    print(n = 100)
+
+
+
+genes_injury %>%
+    # dplyr::select(AKI2_rank, Symb:`pvalInjAA4-AKI2`) %>%
+    dplyr::filter(Symb %in% Hmisc::.q(
+        SLFN5, CYR61, SPARC, CEBPB, ADAM10,
+        VIM, LRRFIP1, TPM4, DUSP1, DOCK11, "HLA-DRB1",
+        KLF6, NRP1, RARRES3, ARL6IP5, FGFR1, MMP7, TSC22D1, WWC1, "HLA-E"
+    ))
+
+
 
 
 genes_injury <- join %>%
